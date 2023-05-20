126d146e9cf9ed052fbf4cb66bad198019c5c74b
Denys <nabajiubass2000@gmail.com>
2023-05-20 19:00:54 +0300

    basic app configuration

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const cors = require("cors");
+const router = require("./router");
 
 const express = require("express");
 
@@ -7,6 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(router);
+
+router.get("/", (req, res) => {
+  res.json({ message: "Hello world" });
+});
 
 function start() {
   app.listen(process.env.PORT, () => {
