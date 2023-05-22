cc640747f31d159cbce781ddca24e8e72f9cf0a9
Inesa Kvitka <inessa.kvitka03@gmail.com>
2023-05-22 23:27:07 +0200

    menu added

--- client/src/pages/Home/index.vue
+++ client/src/pages/Home/index.vue
@@ -1,6 +1,38 @@
 <template>
   <div class="background"></div>
-  <div class="icon"></div>
+  <div class="section">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="/" >ГОЛОВНА</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">НАШІ ПЕРЕВАГИ</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">ПРОКАТ АВТО</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="">ВІДГУКИ</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">ПИТАННЯ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="">КОНТАКТИ</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">МІЙ ПРОФІЛЬ</a>
+  </li>
+    </ul>
+<hr align="left" width="1657" size="2" color="#e0d5c9" />
+</div>
+<div class="span">
+<span style="color: white;">Оренда</span> <span style="color: rgb(255, 217, 0);">Преміум</span> <span style="color: white;">Авто</span> 
+</div>
+<div class="span">
+<span style="color: white; display: inline-block;">+380</span> <span style="color: rgb(255, 217, 0); display: inline-block;">(99)4054553</span>
+</div>
+<div class="icon"></div>
   <div class="yellow-block"></div>
   <div class="car"></div>
 </template>
@@ -21,10 +53,10 @@ export default {};
 }
 .icon {
   position: fixed;
-  top: 2em;
+  top: 2.5em;
   left: 2em;
-  width: 200px; /* Измените размеры изображения на подходящие для вашего случая */
-  height: 50px;
+  width: 150px;
+  height: 39px;
   background-image: url('public/logo.png');
   background-size: cover;
   background-repeat: round;
@@ -47,5 +79,23 @@ export default {};
   background-size: cover;
   background-repeat: no-repeat;
   z-index: 1;
+}
+.section {
+  padding: 1.5em 0em 0em 20em;
+}
+
+.nav-item{
+      padding: 1em 1.5em;
+}
+.nav-link{
+  font-family: 'Century Gothic';
+  font-size: 15px;
+  color: #ffffff;
+}
+.span{
+  font-family: 'Century Gothic';
+  font-size: 45px;
+  text-align: center;
+  font-weight: bold;
 }
 </style>
