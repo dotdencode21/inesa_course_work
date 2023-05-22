5e738070c4d102ba35eb72d32538d9160a7e5eb3
Denys <nabajiubass2000@gmail.com>
2023-05-22 23:26:50 +0300

    added schenas for user and car

---
+++ server/schemas/Car.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const carSchema = new mongoose.Schema({
+  name: String,
+  serialNumber: String,
+  phoneNumber: String,
+  yearOfManufacture: Number,
+  gearboxType: String,
+  fuelType: String,
+  category: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model("Car", carSchema);
